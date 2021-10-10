ae1eea6b765cf69c55981464099624fee98ef89c
yasuharuokazaki <rock.fish.junkie@gmail.com>
2021-10-11 08:16:07 +0900

    add detail

--- polls/views.py
+++ polls/views.py
@@ -3,17 +3,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 
+
+
+
+#add Views
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
-    return HttpResponse(template.render(context,request))
+    return render(request,"polls/index.html",context)
 
 def detail(request,question_id):
-    return HttpResponse("you're looking at question %s."%question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,"polls/detail.html",{"question":question})
+ 
 
 def results(request,question_id):
     response = "You're looking at the results of question %s."
@@ -26,3 +33,6 @@ def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     output = ",".join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
+
+
+    
