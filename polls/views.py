f4b90426edceb540ea65ff623a57e9d2f5007cd7
yasuharuokazaki <rock.fish.junkie@gmail.com>
2021-10-11 09:57:09 +0900

    make vote form

--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,19 @@
 from django.http.request import HttpRequest
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from .models import Question,Choice
 from django.template import loader
-from django.http import Http404
-from django.shortcuts import get_object_or_404
+from django.urls import reverse
 
 
 
 
 #add Views
+
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request,"polls/index.html",context)
+    output = ",".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
@@ -23,16 +21,24 @@ def detail(request,question_id):
  
 
 def results(request,question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,"polls/result.html",{"question":question})
 
 def vote(request,question_id):
-    return HttpResponse("You're voting on question %s."%question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,"polls/detail.html",{
+            "question":question,
+            "error_message":"You did not select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results"),
+        args=(question.id))
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ",".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
 
 
     
