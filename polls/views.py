4dc554b046801efd5d7f141ad7537d0a87aeae8a
yasuharuokazaki <rock.fish.junkie@gmail.com>
2021-10-11 13:42:40 +0900

    make result views

--- polls/views.py
+++ polls/views.py
@@ -24,21 +24,22 @@ def results(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
     return render(request,"polls/result.html",{"question":question})
 
+
+
+
 def vote(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
     try:
-        selected_choice=question.choice_set.get(pk=request.POST["choice"])
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+        
     except (KeyError,Choice.DoesNotExist):
+        
         return render(request,"polls/detail.html",{
             "question":question,
             "error_message":"You did not select a choice."
         })
+
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse("polls:results"),
-        args=(question.id))
-
-
-
-    
+        return HttpResponseRedirect(reverse("polls:results",args=(question.id,))) #args内はtupleなので、必ず','で終わらせる。
