50abb7592fcff1430ac314e9536beb3b18c90ba9
yasuharuokazaki <rock.fish.junkie@gmail.com>
2021-10-14 07:58:57 +0900

    make tests

--- polls/tests.py
+++ polls/tests.py
@@ -3,6 +3,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Question
 
@@ -25,3 +26,39 @@ class QuestionModelTests(TestCase):
         recent_question = Question(pub_date=time)
 
         self.asserIs(recent_question.was_published_recently(),True)
+
+
+
+def create_question(question_text, days):
+  
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        """
+        Questions with a pub_date in the past are displayed on the
+        index page.
+        """
+        question = create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question],
+        )
+
+    def test_future_question(self):
+        create_question(question_text="Future question.",days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response,"No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"],[])
